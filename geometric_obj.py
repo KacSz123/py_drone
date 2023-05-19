02f21ded8c242b3a10ef76191c72b8df3d79a1be
Kacper Szczepanowski <kacperszcz159@gmail.com>
2023-05-19 17:44:23 +0200

    some vector matrix and geomObj methods

--- geometric_obj.py
+++ geometric_obj.py
@@ -1,10 +1,34 @@
 from abc import ABC, abstractmethod
 import numpy as np
-
+import math
+from vector_and_matrix import MatrixNxN, Vector
 class Geometric_obj(ABC):
     __vertexesList=[]
-    __midPoint = np.array([0,0,0])
+    __midPoint = Vector(3)
     __fileHandler = None
+    def moveObject(self, vector):
+        for i in self.__vertexesList:
+            i = i+vector
+
+
+    def rotateObject(self, angle, axis='Z'):
+        if not isinstance(angle, float) or not isinstance(angle, int) or not isinstance(axis,str):
+            print("Angle must be int or float, axis must be one of strings: \"X\",\"Y\", \"Z\"")
+            raise TypeError
+        if axis.upper() != 'Z' or axis.upper() != 'X' or axis.upper() != 'Y': 
+            print(" axis must be one of strings: \"X\",\"Y\", \"Z\" ")
+            raise ValueError
+        
+        m=MatrixNxN(3)
+        if axis.upper() == 'Z':
+            m=m.rot_Z_martrix(angle)
+        if axis.upper() == 'X':
+            m=m.rot_X_martrix(angle)
+        if axis.upper() == 'Y':
+            m=m.rot_Y_martrix(angle)
+        for i in self.__vertexesList:
+            i=i.dot(m)
+        
 
 
     @abstractmethod
--- vector_and_matrix.py
+++ vector_and_matrix.py
@@ -1,32 +1,31 @@
 import numpy as np
 import math
-
-class matrixNxN:
+import sys
+class MatrixNxN:
     def __init__(self, n):
         self.__mSize = n
-        self.__matrix = np.array([[round(0.0,3)]*n]*n)
+        self.__matrix = np.array([[round(0.0,4)]*n]*n)
         # print(self.__matrix)
 
 
     def getMatrix(self):
         return self.__matrix
-    def getsize(self):
-        return self
+    def getSize(self):
+        return self.__mSize
     def __add__(self, other):
-        if isinstance(other,matrixNxN):
+        if isinstance(other,MatrixNxN):
             if self.__mSize != other.__mSize:
-                print('***Matrices are different size!***\n')
-                raise TypeError
+                raise TypeError('***Matrices are different size!***\n')
             self.__matrix = np.add(self.__matrix,other.getMatrix())
         elif isinstance(other, int) or  isinstance(other, float):
-
             self.__matrix = np.add(self.__matrix,other)
         return self
+    
+
     def __sub__(self, other):
-        if isinstance(other,matrixNxN):
+        if isinstance(other,MatrixNxN):
             if self.__mSize != other.__mSize:
-                print('***Matrices are different size!***\n')
-                raise TypeError
+                raise TypeError('***Matrices are different size!***\n')
             self.__matrix = np.subtract(self.__matrix,other.getMatrix())
         elif isinstance(other, int) or  isinstance(other, float):
 
@@ -38,7 +37,7 @@ class matrixNxN:
     def __str__(self) -> str:
         return str(self.__matrix.round(4))
     
-
+    
     def rot_X_martrix(self, angle):
         theta = math.radians(angle)
         c,s = math.cos(round(theta,4)), math.sin(round(theta,4))
@@ -50,8 +49,7 @@ class matrixNxN:
             self.__matrix[0]=[c,s]
             self.__matrix[1]=[-s,c]
         else:
-            print('rotation matrix must have size 2x2 or 3x3')
-            raise ValueError       
+            raise ValueError('rotation matrix must have size 2x2 or 3x3')       
         return self
     def rot_Y_martrix(self, angle):
 
@@ -65,8 +63,7 @@ class matrixNxN:
             self.__matrix[0]=[c,s]
             self.__matrix[1]=[-s,c]
         else:
-            print('rotation matrix must have size 2x2 or 3x3')
-            raise ValueError      
+            raise ValueError('rotation matrix must have size 2x2 or 3x3')      
         return self 
     def rot_Z_martrix(self, angle):
         theta = math.radians(angle)
@@ -79,21 +76,72 @@ class matrixNxN:
             self.__matrix[0]=[c,s]
             self.__matrix[1]=[-s,c]
         else:
-            print('rotation matrix must have size 2x2 or 3x3')
-            raise ValueError     
+            raise ValueError('rotation matrix must have size 2x2 or 3x3')     
         return self   
+
+
+
+
+class Vector():
+    def __init__(self, size, coord=None): 
+        self.__size = size
+        if coord == None:
+            self.__coordinates = np.array([0.0]*size)
+        elif isinstance(coord, list) and len(coord) == size:
+            self.__coordinates = np.array([i for i in coord])
+        else:
+            raise ValueError
+    def getSize(self):
+        return self.__size
+    def getCoords(self):
+        return self.__coordinates
+    def __add__(self, other):
+        if isinstance(other, Vector):
+            if self.__size != other.getSize():
+                raise ValueError('Both Vectors must be the same size!')
+            self.__coordinates = self.__coordinates+other.getCoords()
+        elif isinstance(other, int) or isinstance(other, float):
+            self.__coordinates = self.__coordinates*other
+        else:
+            raise TypeError('Unsuported type')
+        return self
+    
+
+    def __str__(self):
+        return str(self.__coordinates.round(2))
+    def __mul__(self, other):
+        if isinstance(other, MatrixNxN):
+            print(self.__size,'  ',other.getSize())
+            if self.__size != other.getSize():
+                raise ValueError('Matrix and Vector must be the same size!')
+            else:
+                return self.__coordinates.dot(other.getMatrix())
+        elif isinstance(other, Vector):
+            if self.__size != other.getSize():
+
+                raise ValueError('Both Vectors must be the same size!')
+        elif isinstance(other, int) or isinstance(other,float):
+            self.__coordinates=np.dot(self.__coordinates,other)
+        else:
+            raise TypeError
+        return self
+    def __rmul__(self, other):
+        pass
 
 def test():
 
-    m = matrixNxN(3)
-    mm = matrixNxN(3)
+    m = MatrixNxN(3)
+    mm = MatrixNxN(3)
     m = m+3
     m=m.rot_X_martrix(90)
-    print(m)
-    print()
+    vec1,v2,v3 = Vector(2),Vector(3), Vector(3,[1,1,1])
+
     mm.rot_Z_martrix(90)
     print(mm)
-    print()
-    print(m+mm)
+    v2=v3*mm
+    print(v2)
+    # print(mm)
+    # print()
+    # print(m+mm)
 
 test()
